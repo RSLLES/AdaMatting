1f99f2835350bbb2201faac4a40f2f9a4eefa83a
Romain Seailles <rseailles@buf.com>
2021-09-09 16:31:30 +0200

    Fix BGR convention and convert everything to float and create show

--- OwnAdaMatting/dataset.py
+++ OwnAdaMatting/dataset.py
@@ -3,6 +3,10 @@ import pandas as pd
 import tensorflow as tf
 import cv2
 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
 from os.path import join
 
 class AdaMattingDataset:
@@ -35,15 +39,28 @@ class AdaMattingDataset:
 
     def gen_trimap(self, alpha):
         k_size = np.random.choice(range(1, 5))
-        iterations = np.random.randint(1, 20)
+        iterations_d, iterations_e = np.random.randint(1, 15), np.random.randint(1, 15)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (k_size, k_size))
-        dilated = cv2.dilate(alpha, kernel, iterations)
-        eroded = cv2.erode(alpha, kernel, iterations)
-        trimap = np.zeros(alpha.shape)
-        trimap.fill(128)
-        trimap[eroded >= 255] = 255
-        trimap[dilated <= 0] = 0
+        dilated = cv2.dilate(alpha, kernel, iterations_d)
+        eroded = cv2.erode(alpha, kernel, iterations_e)
+        
+        trimap = np.zeros((alpha.shape[0], alpha.shape[1], 3), dtype=np.float32)
+        trimap[:, :, 0][dilated <= 0] = 1.0
+        trimap[:, :, 1][(dilated > 0) & (eroded < 1.0)] = 1.0
+        trimap[:, :, 2][eroded >= 1.0] = 1.0
         return trimap
+
+    def get_transf(self, init_fg_size):
+        def get_rand(size_bg, size_fg):
+            if size_fg == size_bg:
+                return 0
+            return np.random.randint(   low = -int(abs(size_bg - size_fg)/2), 
+                                        high=  int(abs(size_bg - size_fg)/2))
+        return  np.array((
+                    (1, 0,  get_rand(self._size[0], init_fg_size[0])),
+                    (0, 1,  get_rand(self._size[1], init_fg_size[1]))
+                ), dtype=np.float32)
+        
 
     def process(self, line):
         # Ouverture des 3 images
@@ -52,29 +69,41 @@ class AdaMattingDataset:
         bg_img = cv2.imread(join(self._dataset_folder, line[2].numpy().decode('UTF-8')), -1)
 
         # Redimensionnement
+        initial_fg_size = fg_img.shape
         bg_img = cv2.resize(bg_img, dsize=self._size, interpolation=cv2.INTER_NEAREST)
         fg_img      =   self.adapt(fg_img)
         alpha_img   =   self.adapt(alpha_img)
 
         # Deplacement aleatoire
-        M = np.array((
-            (1, 0,  np.random.randint(low = -self._size[0]/2, high=self._size[0]/2)),
-            (0, 1,  np.random.randint(low = -self._size[1]/2, high=self._size[1]/2))
-        ), dtype=np.float32)
-        bg_img      =   bg_img
-        fg_img      =   cv2.warpAffine(fg_img, M, self._size)
-        alpha_img   =   cv2.warpAffine(alpha_img, M, self._size)
-        alpha_img_norm   =   np.expand_dims(alpha_img, axis=-1).astype(np.float)/255
+        M = self.get_transf((initial_fg_size[1], initial_fg_size[0]))
+        bg_img      =   bg_img.astype(np.float32)/255
+        fg_img      =   cv2.warpAffine(fg_img, M, self._size).astype(np.float32)/255
+        alpha_img   =   cv2.warpAffine(alpha_img, M, self._size).astype(np.float32)/255
+        alpha_img_norm   =   np.expand_dims(alpha_img, axis=-1)
         
         # Generating trimap
-
-        x = fg_img*alpha_img_norm + bg_img*(1.0 - alpha_img_norm)
+        x = [   (fg_img*alpha_img_norm + bg_img*(1.0 - alpha_img_norm)),
+                self.gen_trimap(alpha_img)]
 
         # Retour
-        return x.astype(np.uint8)
+        return x, alpha_img
+
+    def show(self, line, with_prediction=False):
+        x, y = self.process(line)
+        fig, axs = plt.subplots(1,3)
+        axs[0].imshow(x[0][:,:,[2,1,0]]) # OpenCV uses BGR convention but Matplotlib uses RGB
+        axs[0].set_title("Patched image")
+
+        axs[1].imshow(y)
+        axs[1].set_title("Real Alpha")
+
+        axs[2].imshow(x[1])
+        axs[2].set_title("User's trimap input")
+
+        plt.legend()
+        plt.show()
 
 
 # Tests
 df = AdaMattingDataset("train", "/net/rnd/DEV/Datasets_DL/alpha_matting/")
-cv2.imshow("test", df.process(next(iter(df._dataset))))
-cv2.waitKey(0)
+df.show(next(iter(df._dataset)))
