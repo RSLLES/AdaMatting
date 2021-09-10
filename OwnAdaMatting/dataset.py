166abce71a3833ee77cd0fe936abe8ad7dbadf2d
Romain Seailles <rseailles@buf.com>
2021-09-10 17:47:23 +0200

    TF version

--- OwnAdaMatting/dataset.py
+++ OwnAdaMatting/dataset.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import tensorflow_addons as tfa
 import cv2
 
 import matplotlib
@@ -10,10 +11,13 @@ import matplotlib.pyplot as plt
 from os.path import join
 
 class AdaMattingDataset:
-    def __init__(self, mode, dataset_folder) -> None:
-        self._size = (1280, 720)
+    def __init__(self, mode, dataset_folder, img_size = (1024, 1024)) -> None:
+        self._size = img_size
+        self._size_tf = (img_size[1], img_size[0]) # Tensorflow convension
+
         self._aspect_ratio = self._size[0]/self._size[1]
         self._dataset_folder = dataset_folder
+        self._dataset_folder_tf = tf.constant(dataset_folder)
         self._mode = mode
 
         self._path_data = join(dataset_folder, mode, "data.csv")
@@ -27,81 +31,92 @@ class AdaMattingDataset:
         self._dataset = self._dataset.map(parse_csv)
 
     def adapt(self, img):
-        deltaW = self._size[0] - img.shape[1] # Shape is (rows, cols) which in image language is (height, weidht)
-        deltaH = self._size[1] - img.shape[0]
-        if deltaW > 0:
-             img = cv2.copyMakeBorder(img, 0, 0, int(deltaW/2), deltaW - int(deltaW/2), cv2.BORDER_CONSTANT)
+        deltaH = self._size_tf[0] - img.shape[-3]
+        deltaW = self._size_tf[1] - img.shape[-2]
         if deltaH > 0:
-             img = cv2.copyMakeBorder(img, int(deltaH/2), deltaH - int(deltaH/2), 0, 0, cv2.BORDER_CONSTANT)
+            img = tf.image.pad_to_bounding_box(img, int(deltaH/2), 0, self._size_tf[0], img.shape[-2])
+        if deltaW > 0:
+            img = tf.image.pad_to_bounding_box(img, 0, int(deltaW/2), img.shape[-3], self._size_tf[1])
         if deltaH < 0 or deltaW < 0:
-            cv2.resize(img, self._size, interpolation=cv2.INTER_NEAREST)
+            img = tf.image.resize_with_pad(img, self._size_tf[0], self._size_tf[1], tf.image.ResizeMethod.NEAREST_NEIGHBOR)
         return img
 
     def gen_trimap(self, alpha):
-        k_size = np.random.choice(range(1, 5))
-        iterations_d, iterations_e = np.random.randint(1, 15), np.random.randint(1, 15)
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (k_size, k_size))
-        dilated = cv2.dilate(alpha, kernel, iterations_d)
-        eroded = cv2.erode(alpha, kernel, iterations_e)
+        alpha_exp = tf.expand_dims(alpha, axis=0)
+        kernel_size_d = tf.random.uniform(shape=[2], minval=0, maxval=50, dtype=tf.int64)
+        kernel_size_e = tf.random.uniform(shape=[2], minval=0, maxval=20, dtype=tf.int64)
+
+        dilated = tf.nn.max_pool2d(alpha_exp, ksize=kernel_size_d, strides=1, padding="SAME")
+        eroded = -tf.nn.max_pool2d(-alpha_exp, ksize=kernel_size_e, strides=1, padding="SAME")
         
-        trimap = np.zeros((alpha.shape[0], alpha.shape[1], 3), dtype=np.float32)
-        trimap[:, :, 0][dilated <= 0] = 1.0
-        trimap[:, :, 1][(dilated > 0) & (eroded < 1.0)] = 1.0
-        trimap[:, :, 2][eroded >= 1.0] = 1.0
-        return trimap
+        trimap = tf.concat([
+            tf.cast(dilated <= 0, dtype=tf.float32),
+            tf.cast((dilated > 0) & (eroded < 1), dtype=tf.float32),
+            tf.cast(eroded >= 1, dtype=tf.float32)
+        ], axis=-1)
+        return tf.squeeze(trimap, axis=0)
+
+    def extract_trimap(self, alpha):
+        return tf.concat([
+            tf.cast(alpha <= 0, dtype=tf.float32),
+            tf.cast((alpha > 0) & (alpha < 1), dtype=tf.float32),
+            tf.cast(alpha >= 1, dtype=tf.float32)
+        ], axis=-1)
 
     def get_transf(self, init_fg_size):
         def get_rand(size_bg, size_fg):
             if size_fg == size_bg:
-                return 0
-            return np.random.randint(   low = -int(abs(size_bg - size_fg)/2), 
-                                        high=  int(abs(size_bg - size_fg)/2))
-        return  np.array((
-                    (1, 0,  get_rand(self._size[0], init_fg_size[0])),
-                    (0, 1,  get_rand(self._size[1], init_fg_size[1]))
-                ), dtype=np.float32)
+                return tf.constant(0)
+            return tf.cast(tf.random.uniform(shape=[], minval=-int(abs(size_bg - size_fg)/2), maxval= int(abs(size_bg - size_fg)/2), dtype=tf.int64), dtype=tf.float32)
+        return  tf.concat([get_rand(self._size_tf[1], init_fg_size[-2]), get_rand(self._size_tf[0], init_fg_size[-3])], axis=0)
         
 
     def process(self, line):
         # Ouverture des 3 images
-        fg_img = cv2.imread(join(self._dataset_folder, line[0].numpy().decode('UTF-8')), -1)
-        alpha_img = cv2.imread(join(self._dataset_folder, line[1].numpy().decode('UTF-8')), -1)
-        bg_img = cv2.imread(join(self._dataset_folder, line[2].numpy().decode('UTF-8')), -1)
+        def open_img (path) : 
+            return tf.image.convert_image_dtype(tf.image.decode_jpeg(tf.io.read_file(tf.strings.join([self._dataset_folder_tf, path]))), dtype=float)
+        
+        fg_img =        open_img(line[0])
+        alpha_img =     open_img(line[1])
+        bg_img =        open_img(line[2])
 
         # Redimensionnement
         initial_fg_size = fg_img.shape
-        bg_img = cv2.resize(bg_img, dsize=self._size, interpolation=cv2.INTER_NEAREST)
+        bg_img      =   tf.image.resize(bg_img, self._size_tf)
         fg_img      =   self.adapt(fg_img)
         alpha_img   =   self.adapt(alpha_img)
 
         # Deplacement aleatoire
-        M = self.get_transf((initial_fg_size[1], initial_fg_size[0]))
-        bg_img      =   bg_img.astype(np.float32)/255
-        fg_img      =   cv2.warpAffine(fg_img, M, self._size).astype(np.float32)/255
-        alpha_img   =   cv2.warpAffine(alpha_img, M, self._size).astype(np.float32)/255
-        alpha_img_norm   =   np.expand_dims(alpha_img, axis=-1)
+        deplacement =   self.get_transf(initial_fg_size)
+        fg_img      =   tfa.image.translate(fg_img, translations=deplacement, fill_mode="constant")
+        alpha_img   =   tfa.image.translate(alpha_img, translations=deplacement, fill_mode="constant")
         
         # Generating trimap
-        x = [   (fg_img*alpha_img_norm + bg_img*(1.0 - alpha_img_norm)),
+        x = [   (fg_img*alpha_img + bg_img*(1.0 - alpha_img)),
                 self.gen_trimap(alpha_img)]
 
         # Retour
-        return x, alpha_img
+        return x, self.extract_trimap(alpha_img)
 
     def show(self, line, with_prediction=False):
         x, y = self.process(line)
         fig, axs = plt.subplots(1,3)
-        axs[0].imshow(x[0][:,:,[2,1,0]]) # OpenCV uses BGR convention but Matplotlib uses RGB
+        axs[0].imshow(x[0])
         axs[0].set_title("Patched image")
 
         axs[1].imshow(y)
-        axs[1].set_title("Real Alpha")
+        axs[1].set_title("Real Trimap")
 
         axs[2].imshow(x[1])
         axs[2].set_title("User's trimap input")
 
         plt.legend()
         plt.show()
+
+    def get_dataset(self, batch_size, threads=5, shuffle_buffer=10000, repeat=5):
+        self._dataset = self._dataset.shuffle(shuffle_buffer).repeat(repeat)
+        self._dataset = self._dataset.map(lambda line : self.process(line), num_parallel_calls=threads)
+        return self._dataset.batch(batch_size=batch_size).prefetch(1)
 
 
 # Tests
