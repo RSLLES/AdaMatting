fix loss (La with softmax) + GRU propagation

OwnAdaMatting/main.py
@@ -13,7 +13,7 @@ from tensorflow.keras.optimizers import Adam
 # physical_devices = tf.config.list_physical_devices("GPU")
 # tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-from keras.utils.vis_utils import plot_model
+# from keras.utils.vis_utils import plot_model
 
 from network import get_model, MultiTaskLoss
 from dataset import AdaMattingDataset
@@ -24,12 +24,11 @@ from utils import image_grid, plot_to_image, generate_graph
 ### VARIABLES ###
 #################
 
-img_size = (512, 512)
-batch_size = 15
+img_size = (256, 256)
+batch_size = 25
 N_EPOCHS = 150
-PERIOD_TEST = 60*20 # Temps en seconde entre chaque test
+PERIOD_TEST = 60*1 # Temps en seconde entre chaque test
 last_test = time()
-
 
 ###################
 ### PREPARATION ###
@@ -41,11 +40,11 @@ while not succeed:
         print(date)
         log_dir = f'OwnAdaMatting/logs/{date}/'
         save_dir = f'OwnAdaMatting/saves/{date}/'
-        os.mkdir(save_dir)
+
         df = AdaMattingDataset("train", "/net/rnd/DEV/Datasets_DL/alpha_matting/", img_size=img_size, batch_size=batch_size)
         model = get_model(img_size=img_size, depth=16)
         loss_function = MultiTaskLoss()
-        opt = Adam(learning_rate=0.0001)
+        opt = Adam(learning_rate=0.001)
         # model.compile(
         #     optimizer=Adam(learning_rate=0.0001),
         #     loss=MultiTaskLoss(),
@@ -66,7 +65,6 @@ while not succeed:
         i = 0
         img_index = 0
         for epoch in range(N_EPOCHS):
-            model.save_weights(join(save_dir, datetime.now().strftime("%m-%d_%Hh%M") + ".h5"), save_format="h5")
             progress_bar = tqdm(df._df_train, desc=f"epoch={epoch}")
             progress_bar.set_postfix({"loss" : None})
             for x_batch, y_batch in progress_bar:
@@ -77,7 +75,13 @@ while not succeed:
 
                 gradients = tape.gradient(loss, model.trainable_weights)
                 opt.apply_gradients(zip(gradients, model.trainable_weights))
-                progress_bar.set_postfix({"loss" : str(loss.numpy())})
+                s1 = f"{tf.exp(0.5*model.layers[-1].kernel[0]).numpy()[0]:.4f}"
+                s2 = f"{tf.exp(0.5*model.layers[-1].kernel[1]).numpy()[0]:.4f}"
+                loss_str = f"{loss.numpy():.4f}"
+                progress_bar.set_postfix({
+                    "loss" : loss_str,
+                    "s1" : s1,
+                    "s2" : s2})
 
                 # Logging training data
                 with train_writer.as_default():
@@ -85,6 +89,10 @@ while not succeed:
 
                 #  Logging testing and images
                 if time() - last_test > PERIOD_TEST:
+                    if not os.path.exists(save_dir):
+                        os.mkdir(save_dir)
+        
+                    model.save_weights(join(save_dir, datetime.now().strftime("%m-%d_%Hh%M") + ".h5"), save_format="h5")
                     last_test = time()
                     Loss, Acc, Mse = [],[],[]
                     for x_batch, y_batch in df._df_test:
@@ -111,5 +119,5 @@ while not succeed:
                 i+=1
         succeed = True
     except tf.errors.ResourceExhaustedError as e:
-        batch_size = max(1, int(batch_size*0.75))
+        batch_size = max(1, batch_size-1)
         print(f"Got OOM : reducing batch size to {batch_size}")

OwnAdaMatting/network.py
@@ -1,11 +1,11 @@
+from keras.layers.core import Activation
 from keras.losses import BinaryCrossentropy
 import tensorflow as tf
 from keras.layers import Input, Conv2D, BatchNormalization, Lambda, MaxPooling2D, Add, Concatenate, Layer
-from keras.layers.convolutional_recurrent import ConvLSTM, ConvLSTMCell, ConvRNN
 
 from keras.layers.advanced_activations import LeakyReLU, Softmax
 from keras import Model
-from keras import regularizers
+from keras.initializers import Constant
 from keras.backend import expand_dims
 from keras.losses import Loss
 
@@ -46,9 +46,11 @@ class ConvBNRelu (Layer):
         return (input_shape[0], input_shape[1], input_shape[2], self.depth)
 
 class ResBlock (Layer):
-    def __init__(self, downsample=False, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
+    def __init__(self, depth=None, downsample=False, kernel=3, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
         self.downsample = downsample
         self.stride = 2 if downsample else 1
+        self.depth = depth
+        self.kernel = kernel
         super().__init__(trainable=trainable, name=name, dtype=dtype, dynamic=dynamic, **kwargs)
 
     def get_config(self):
@@ -56,13 +58,18 @@ class ResBlock (Layer):
         return {
             **base_config,
             "downsample" : self.downsample,
-            "stride" : self.stride}
+            "stride" : self.stride,
+            "depth" : self.depth,
+            "kernel" : self.kernel}
 
     def build(self, input_shape):
-        self.depth = input_shape[-1]*2 if self.downsample else None
+        if self.depth == None:
+            self.depth = input_shape[-1]*2 if self.downsample else self.depth
+        else:
+            self.depth = self.depth*2 if self.downsample else self.depth
         
-        self.conv1 = ConvBNRelu(depth=self.depth, kernel=3, stride=self.stride)
-        self.conv2 = ConvBNRelu(depth=None, kernel=3, stride=1)
+        self.conv1 = ConvBNRelu(depth=self.depth, kernel=self.kernel, stride=self.stride)
+        self.conv2 = ConvBNRelu(depth=None, kernel=self.kernel, stride=1)
         self.convcut = ConvBNRelu(depth=self.depth, kernel=1, stride=self.stride)
         self.add = Add()
         self.relu = LeakyReLU()
@@ -132,89 +139,74 @@ class SubPixelConv (Layer):
     def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[1]*2, input_shape[2]*2, int(self.depth/4))
 
-class PropagationCell (ConvLSTMCell):
+class PropagationUnit (Layer):
+    def __init__(self, depth=None, kernel=3, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
+        self.depth = depth
+        self.kernel = kernel
+        super().__init__(trainable=trainable, name=name, dtype=dtype, dynamic=dynamic, **kwargs)
+
+    def get_config(self):
+        base_config = super().get_config()
+        return {
+            **base_config,
+            "depth" : self.depth,
+            "kernel" : self.kernel}
+
     def build(self, input_shape):
-        self.rs = ResBlock(downsample=False)
-        self.conv = ConvBNRelu(depth=12) # 12 = 3 (img) + 3 (trimap donnee) + 3 (trimap refined) + 3 (decoder alpha)
-        super().build(input_shape)
+        _, _, alpha_estimation_shape = input_shape
+        self.depth = alpha_estimation_shape[-1] if self.depth == None else self.depth
 
-    def call(self, inputs, states, *args, **kwargs):
-        last_alpha = states[0]
-        l = Concatenate(axis=-1)([inputs, last_alpha])
-        l = self.rs(l)
-        l = self.conv(l)
-        return super().call(l, states, *args, **kwargs)
+        self.preprocess_i = ResBlock()
+        self.concatenate_img_trimap_alpha = Concatenate(axis=-1)
+        self.concatenate_hi = Concatenate(axis=-1)
+        self.concatenate_ui = Concatenate(axis=-1)
+        self.sigm = Activation("sigmoid")
+        self.activation = Activation("tanh")
+        self.conv_z = Conv2D(self.depth, kernel_size=self.kernel, padding="same")
+        self.conv_r = Conv2D(self.depth, kernel_size=self.kernel, padding="same")
+        self.conv_hh = Conv2D(self.depth, kernel_size=self.kernel, padding="same")
 
-class Propagation (ConvLSTM):
-    def __init__(self,
-               filters,
-               kernel_size,
-               strides=1,
-               padding='valid',
-               data_format=None,
-               dilation_rate=1,
-               activation='tanh',
-               recurrent_activation='hard_sigmoid',
-               use_bias=True,
-               kernel_initializer='glorot_uniform',
-               recurrent_initializer='orthogonal',
-               bias_initializer='zeros',
-               unit_forget_bias=True,
-               kernel_regularizer=None,
-               recurrent_regularizer=None,
-               bias_regularizer=None,
-               activity_regularizer=None,
-               kernel_constraint=None,
-               recurrent_constraint=None,
-               bias_constraint=None,
-               return_sequences=False,
-               return_state=False,
-               go_backwards=False,
-               stateful=False,
-               dropout=0.0,
-               recurrent_dropout=0.0,
-               **kwargs):
-        cell = PropagationCell(
-            rank=2,
-            filters=filters,
-            kernel_size=kernel_size,
-            strides=strides,
-            padding=padding,
-            data_format=data_format,
-            dilation_rate=dilation_rate,
-            activation=activation,
-            recurrent_activation=recurrent_activation,
-            use_bias=use_bias,
-            kernel_initializer=kernel_initializer,
-            recurrent_initializer=recurrent_initializer,
-            bias_initializer=bias_initializer,
-            unit_forget_bias=unit_forget_bias,
-            kernel_regularizer=kernel_regularizer,
-            recurrent_regularizer=recurrent_regularizer,
-            bias_regularizer=bias_regularizer,
-            kernel_constraint=kernel_constraint,
-            recurrent_constraint=recurrent_constraint,
-            bias_constraint=bias_constraint,
-            dropout=dropout,
-            recurrent_dropout=recurrent_dropout,
-            dtype=kwargs.get('dtype'))
-        super(ConvLSTM, self).__init__(
-            2,
-            cell,
-            return_sequences=return_sequences,
-            return_state=return_state,
-            go_backwards=go_backwards,
-            stateful=stateful,
-            **kwargs)
-        self.activity_regularizer = regularizers.get(activity_regularizer)
+    def call(self, inputs, *args, **kwargs):
+        input_img_and_trimap,  adapted_trimap, current_alpha_estimation = inputs
 
+        # Remove input user's trimap
+        input_img = tf.slice(input_img_and_trimap, [0,0,0,0],[-1, -1, -1, 3])
+
+        # Preprocess
+        i = self.concatenate_img_trimap_alpha([input_img, adapted_trimap, current_alpha_estimation])
+        i = self.preprocess_i(i)
+
+        # Concatenate h and i
+        h = current_alpha_estimation
+        hi = self.concatenate_hi([h, i])
+
+        # Update Gate
+        z = self.sigm(self.conv_z(hi))
+
+        # Reset Gate
+        r = self.sigm(self.conv_r(hi))
+
+        # Candidate Alpha
+        u = r*h
+        ui = self.concatenate_ui([u, i])
+        hh = self.activation(self.conv_hh(ui))
+
+        # Merging
+        return (1-z)*h + z*hh
+
+
+    def compute_output_shape(self, input_shape):
+        _, _, alpha_estimation_shape = input_shape
+        return alpha_estimation_shape
+    
 #############################
 ### Custom Multitask Loss ###
 #############################
 
 class Weights(Layer):
-    def __init__(self, output_dim, **kwargs):
+    def __init__(self, output_dim, initial_value=1.0, **kwargs):
        self.output_dim = output_dim
+       self.initial_value = initial_value
        super(Weights, self).__init__(**kwargs)
 
     def get_config(self):
@@ -222,10 +214,15 @@ class Weights(Layer):
         return {
             **base_config,
             "kernel" : self.kernel,
-            "output_dim" : self.output_dim}
+            "output_dim" : self.output_dim,
+            "initial_value" : self.initial_value}
 
     def build(self, input_shapes):
-       self.kernel = self.add_weight(name='kernel', shape=self.output_dim, initializer='uniform', trainable=True)
+       self.kernel = self.add_weight(
+           name='kernel', 
+           shape=self.output_dim, 
+           initializer=Constant(self.initial_value), 
+           trainable=True)
        super(Weights, self).build(input_shapes)  
 
     def call(self, inputs=None):
@@ -240,25 +237,25 @@ class MultiTaskLoss(Loss):
         self.bce = BinaryCrossentropy(from_logits=False)
         super().__init__(reduction=reduction, name=name)
 
-    def __call__(self, y_true, y_pred, sample_weight=None):
+    def __call__(self, y_true, y_pred, sample_weight=None, eps=1e-6):
         gt_trimap = tf.slice(y_true, [0,0,0,0], [-1, -1, -1, 3])
         gt_alpha =  tf.slice(y_true, [0,0,0,3], [-1, -1, -1, 1])
         trimap, alpha, weights = y_pred
 
-        log_s1 = tf.squeeze(tf.slice(weights, [0,0], [1,1]))
-        log_s2 = tf.squeeze(tf.slice(weights, [1,0], [1,1]))
-        s1_2 = tf.square(tf.exp(log_s1))
-        s2 = tf.exp(log_s2)
+        log_s1_sqr = tf.squeeze(tf.slice(weights, [0,0], [1,1]))
+        log_s2_sqr = tf.squeeze(tf.slice(weights, [1,0], [1,1]))
+        s1_sqr = tf.exp(log_s1_sqr)
+        s2_sqr = tf.exp(log_s2_sqr)
 
         # Loss de l'alpha (ponderes par les pixels gris estimes)
         grey = tf.slice(trimap, [0,0,0,1], [-1, -1, -1, 1])
-        loss_alpha = tf.reduce_sum(tf.abs(alpha - gt_alpha)*grey)/tf.reduce_sum(grey)
+        loss_alpha = tf.reduce_sum(tf.abs(alpha - gt_alpha)*grey)/(tf.reduce_sum(grey) + eps) + eps
 
         # Loss de la trimap : Binary cross entropy
         loss_trimap = self.bce(y_true=gt_trimap, y_pred=trimap)
 
         # Fusion
-        return 0.5*loss_trimap/s1_2 + loss_alpha/s2 + log_s1 + log_s2
+        return loss_trimap/(s1_sqr + eps) + loss_alpha/(s2_sqr + eps) + log_s1_sqr + log_s2_sqr
         
 
 ########################
@@ -331,21 +328,21 @@ def get_model(img_size, depth=32):
     l = DecoderBlock(kernel=3, double_reduction=False)(l)
 
     # Sortie
-    l = Conv2D(3, kernel_size=3, padding="same", name="conv_out_alpha")(l)
-    end_alpha_decoder = Softmax()(l)
-
+    l = Conv2D(1, kernel_size=3, padding="same", name="conv_out_alpha")(l)
+    end_alpha_decoder = Activation("sigmoid")(l)
 
     ########################
     ### Propagation Unit ###
     ########################
-    all = Concatenate(axis=-1)([inputs, end_alpha_decoder, end_trimap_decoder])
-    all_with_t = expand_dims(all, axis=1)
-    all_through_time = Concatenate(axis=1)([all_with_t, all_with_t, all_with_t])
-    out = Propagation(filters=1, kernel_size=3, padding="same")(all_through_time)
+    prop = PropagationUnit(depth=1, kernel=7)
+    alpha = end_alpha_decoder
+    for _ in range(3):
+        alpha = prop([inputs, end_trimap_decoder, alpha])
+    out = alpha
 
     #########################
     ### Add Loss' Weights ###
     #########################
-    loss_weights = Weights((2,1))(inputs)
+    loss_weights = Weights(output_dim=(2,1), initial_value=2.0*tf.math.log(4.0))(inputs)
 
     return Model(inputs=inputs, outputs=[end_trimap_decoder, out, loss_weights], name="trimap_decoder")