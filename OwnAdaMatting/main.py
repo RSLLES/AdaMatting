aba9554e27154446146134c5a6943af0ceb4fab8
Romain Seailles <rseailles@buf.com>
2021-10-15 12:17:17 +0200

    12h15 from scrath with full length, lr=0.0001 and full alpha loss

--- OwnAdaMatting/main.py
+++ OwnAdaMatting/main.py
@@ -54,7 +54,7 @@ while not succeed:
         # df = LiveComputedDataset("all_files", "/net/rnd/DEV/Datasets_DL/alpha_matting/", img_size=img_size, batch_size=batch_size)
         df = DeepDataset("/net/rnd/DEV/Datasets_DL/alpha_matting/deep38/", batch_size=batch_size, img_size=img_size, size_dividor=32, max_size_factor=3)
         model, observers = get_model(img_size=img_size, depth=32)
-        model.load_weights("/net/homes/r/rseailles/Deep/OwnAdaMatting/saves/10-15_09h17/10-15_09h37.h5")
+        # model.load_weights("/net/homes/r/rseailles/Deep/OwnAdaMatting/saves/10-15_09h17/10-15_09h37.h5")
         opt = Adam(learning_rate=0.0001)
         
         loss_alpha_func = AlphaLoss()
